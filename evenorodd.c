ab47e18487e30e56b38339eb384236e9ab82f586
dhroovrawal2005 <dhroovrawal2005@gmail.com>
2026-09-01 11:33:40 +0530

    Add files via upload

---
+++ evenorodd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int A;
+
+    printf("write any number A: ");
+    scanf("%d",&A);
+
+    if (A%2==0) {
+    printf("A is even\n");
+    } else {
+    printf("A is odd");
+}
+
+    return 0;
+
+
+}
